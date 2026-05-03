4ee362a55d1afbe1e35691f3d172308e6c500ba1
mat-analytics <mateuszfibich13@gmail.com>
2026-05-03 11:46:26 +0200

    Delete sql directory

--- sql/01_avg_goals_per_league.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    l.name AS liga,
-    COUNT(*) AS liczba_mecze,
-    ROUND(AVG(m.home_team_goal + m.away_team_goal), 2) AS srednia_goli
-FROM Match m
-JOIN League l ON m.league_id = l.id
-GROUP BY l.name
-ORDER BY srednia_goli DESC;
