2beb9ec99cee6e335f640b416bad8bfb0a62b844
mat-analytics <mateuszfibich13@gmail.com>
2026-05-03 11:45:11 +0200

    Delete sql/sql/02_home_advantage_per_league.sql

--- sql/sql/02_home_advantage_per_league.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    l.name AS liga,
-    ROUND(100.0 * SUM(CASE WHEN m.home_team_goal > m.away_team_goal THEN 1 ELSE 0 END) / COUNT(*), 1) AS procent_wygranych_gospodarz,
-    ROUND(100.0 * SUM(CASE WHEN m.home_team_goal < m.away_team_goal THEN 1 ELSE 0 END) / COUNT(*), 1) AS procent_wygranych_gosc,
-    ROUND(100.0 * SUM(CASE WHEN m.home_team_goal = m.away_team_goal THEN 1 ELSE 0 END) / COUNT(*), 1) AS procent_remisow
-FROM Match m
-JOIN League l ON m.league_id = l.id
-GROUP BY l.name
-ORDER BY procent_wygranych_gospodarz DESC;
